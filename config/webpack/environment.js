31cc12ae6c4d51b40e095441f9e6c4494f43dabf
Sandeep <sandeeppradhan313@gmail.com>
2020-05-19 14:00:26 +0530

    Final Project Added

---
+++ config/webpack/environment.js
@@ -0,0 +1,16 @@
+const { environment } = require("@rails/webpacker");
+// const coffee = require("./loaders/coffee");
+var webpack = require("webpack");
+environment.plugins.prepend(
+  "Provide",
+  new webpack.ProvidePlugin({
+    $: "jquery/src/jquery",
+    jQuery: "jquery/src/jquery",
+    jquery: "jquery",
+    "window.jQuery": "jquery",
+    Popper: ["popper.js", "default"],
+  })
+);
+
+// environment.loaders.prepend("coffee", coffee);
+module.exports = environment;
